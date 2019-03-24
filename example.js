4bcff6d04b5efcda21d4c789261db768edab7d29
Tim <luckystudydanit@gmail.com>
2019-03-24 11:28:58 +0200

    Denny first_changes 24/03/2019

---
+++ example.js
@@ -0,0 +1,13 @@
+{/* <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/modernizr-custom.js"></script>
+Modernizr.on('webp', function (result) {
+  if (result) {
+        $("[data-webp]").each(function(i,elem) {
+            $(elem).css('background-image', 'url(' + $(elem).attr("data-webp") + ')');
+        });
+  }
+  else {
+        $("[data-img]").each(function(i,elem) {
+            $(elem).css('background-image', 'url(' + $(elem).attr("data-img") + ')');
+        });
+  }
+}); */}
