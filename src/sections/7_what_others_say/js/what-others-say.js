c720ac4183bbea0d9e6bb7fb860b4bb50aaf9315
IrynaViatkina <vis720.mail.ru>
2019-04-05 08:15:43 +0300

    section_structure

--- src/sections/7_what_others_say/js/what-others-say.js
+++ src/sections/7_what_others_say/js/what-others-say.js
@@ -1,1 +1,40 @@
-console.log('what-others-say');
+let peoplePhoto = document.getElementsByClassName("photo-item");
+let peopleSpeach = document.getElementsByClassName("people-speach");
+let screens = document.getElementsByClassName("whatsapp-block-item");
+
+let whatsappBlock = document.getElementById("whatsapp-block");
+let overlay = document.getElementById("overlay");
+
+let activeBlock = 0;
+
+document.addEventListener('click', function(e){
+    if(e.target.id === 'right-arrow' || e.target.id === 'right-arrow-screen'){
+        if(activeBlock !== peoplePhoto.length - 1){
+            activeBlock++;
+            movePhotoBlock(activeBlock, activeBlock - 1);
+        }
+    }
+    if(e.target.id === 'left-arrow' || e.target.id === 'left-arrow-screen'){
+        if(activeBlock !== 0){
+            activeBlock--;
+            movePhotoBlock(activeBlock, activeBlock + 1);
+        }
+    }
+    if(e.target.id === "whatsapp-btn"){
+        whatsappBlock.hidden = false;
+        overlay.hidden = false;
+    }
+    if(e.target.id === "close"){
+        whatsappBlock.hidden = true;
+        overlay.hidden = true;
+    }
+});
+
+function movePhotoBlock(activeItem, hiddenItem){
+    peoplePhoto[activeItem].hidden = false;
+    peopleSpeach[activeItem].hidden = false;
+    screens[activeItem].hidden = false;
+    peoplePhoto[hiddenItem].hidden = true;
+    peopleSpeach[hiddenItem].hidden = true;
+    screens[hiddenItem].hidden = true;
+}
