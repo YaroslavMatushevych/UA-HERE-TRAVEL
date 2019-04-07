33c818e4af549c62edf56fb16efeef23155ce63a
MishaYelysieiev <yelymykh@gmail.com>
2019-04-08 01:50:42 +0300

    fixed multiple choice bug and added input error message

--- src/sections/5_tell_us_about/js/tell-us-about.js
+++ src/sections/5_tell_us_about/js/tell-us-about.js
@@ -1,5 +1,6 @@
-
 $(document).ready(function () {
+
+    ///////////VARIABLES/////////////////
 
     const kyivVisit = $('#kyiv-been');
     const arriveDate = $('#arrive-date');
@@ -8,106 +9,156 @@ $(document).ready(function () {
     const preferenceChoose = $('#preferences-choose');
     const budgetChoose = $('#budget');
     const tasteChoose = $('#taste');
+    const today = new Date().toISOString().substr(0, 10);
 
-    let hideErrorMessage=()=>{
-        if(kyivVisit.hasClass('filled-quest')){
-            kyivVisit.find('.form-error').css({'opacity':'0'});
-        };
-        if(arriveDate.val()){
-            arriveDate.siblings('.form-error').css({'opacity':'0'});
-        };
-        if(daysVisit.hasClass('filled-quest')){
-            daysVisit.find('.form-error').css({'opacity':'0'});
+
+    ///////////FUNCTIONS/////////////////
+
+    let hideErrorMessage = () => {
+        if (kyivVisit.hasClass('filled-quest')) {
+            kyivVisit.parent().find('.form-error').css({'opacity': '0'});
         }
-        if(childrenVisit.hasClass('filled-quest')){
-            childrenVisit.find('.form-error').css({'opacity':'0'});
+        ;
+        if (arriveDate.val()) {
+            arriveDate.siblings('.form-error').css({'opacity': '0'});
         }
-        if(preferenceChoose.hasClass('filled-quest')){
-            preferenceChoose.find('.form-error').css({'opacity':'0'});
+        ;
+        if (daysVisit.hasClass('filled-quest')) {
+            daysVisit.parent().find('.form-error').css({'opacity': '0'});
         }
-        if(budgetChoose.hasClass('filled-quest')){
-            budgetChoose.find('.form-error').css({'opacity':'0'});
+        if (childrenVisit.hasClass('filled-quest')) {
+            childrenVisit.parent().find('.form-error').css({'opacity': '0'});
         }
-        if(tasteChoose.hasClass('filled-quest')){
-            tasteChoose.find('.form-error').css({'opacity':'0'});
+        if (preferenceChoose.hasClass('filled-quest')) {
+            preferenceChoose.find('.form-error').css({'opacity': '0'});
+        }
+        if (budgetChoose.hasClass('filled-quest')) {
+            budgetChoose.parent().find('.form-error').css({'opacity': '0'});
+        }
+        if (tasteChoose.hasClass('filled-quest')) {
+            tasteChoose.find('.form-error').css({'opacity': '0'});
+        }
+        if ($('#customer-name').val()) {
+            $('#customer-name').siblings('.name-input-error').css('opacity', '0');
+        }
+        ;
+        if ($('#customer-email').val()) {
+            $('#customer-email').siblings('.mail-input-error').css('opacity', '0');
+        }
+        ;
+    };
+
+    let showErrorMessageFirst = () => {
+        if (!kyivVisit.hasClass('filled-quest')) {
+            kyivVisit.parent().find('.form-error').css({'opacity': '1'});
+        }
+        ;
+        if (!arriveDate.val()) {
+            arriveDate.siblings('.form-error').css({'opacity': '1'});
+        }
+        ;
+        if (!daysVisit.hasClass('filled-quest')) {
+            daysVisit.parent().find('.form-error').css({'opacity': '1'});
         }
     };
 
-    let showErrorMessageFirst=()=>{
-        if(!kyivVisit.hasClass('filled-quest')){
-            kyivVisit.find('.form-error').css({'opacity':'1'});
-        };
-        if(!arriveDate.val()){
-            arriveDate.siblings('.form-error').css({'opacity':'1'});
-        };
-        if(!daysVisit.hasClass('filled-quest')){
-            daysVisit.find('.form-error').css({'opacity':'1'});
+    let showErrorMessageSecond = () => {
+        if (!childrenVisit.hasClass('filled-quest')) {
+            childrenVisit.parent().find('.form-error').css({'opacity': '1'});
+        }
+        if (!preferenceChoose.hasClass('filled-quest')) {
+            preferenceChoose.find('.form-error').css({'opacity': '1'});
+        }
+        if (!budgetChoose.hasClass('filled-quest')) {
+            budgetChoose.parent().find('.form-error').css({'opacity': '1'});
+        }
+        if (!tasteChoose.hasClass('filled-quest')) {
+            tasteChoose.find('.form-error').css({'opacity': '1'});
         }
     };
 
-    let showErrorMessageSecond=()=>{
-        if(!childrenVisit.hasClass('filled-quest')){
-            childrenVisit.find('.form-error').css({'opacity':'1'});
-        }
-        if(!preferenceChoose.hasClass('filled-quest')){
-            preferenceChoose.find('.form-error').css({'opacity':'1'});
-        }
-        if(!budgetChoose.hasClass('filled-quest')){
-            budgetChoose.find('.form-error').css({'opacity':'1'});
-        }
-        if(!tasteChoose.hasClass('filled-quest')){
-            tasteChoose.find('.form-error').css({'opacity':'1'});
-        }
-    };
-
-    let checkFilledForm =()=>{
-        if((kyivVisit.hasClass('filled-quest')) && (daysVisit.hasClass('filled-quest')) && ($('#arrive-date').val())){
+    let checkFilledForm = () => {
+        if ((kyivVisit.hasClass('filled-quest')) && (daysVisit.hasClass('filled-quest')) && ($('#arrive-date').val())) {
             $('#next-1').addClass('active-next-btn');
-        };
-        if((childrenVisit.hasClass('filled-quest')) && (preferenceChoose.hasClass('filled-quest')) && (budgetChoose.hasClass('filled-quest')) && (tasteChoose.hasClass('filled-quest'))){
+        } else {
+            $('#next-1').removeClass('active-next-btn');
+        }
+        if ((childrenVisit.hasClass('filled-quest')) && (preferenceChoose.hasClass('filled-quest')) && (budgetChoose.hasClass('filled-quest')) && (tasteChoose.hasClass('filled-quest'))) {
             $('#next-2').addClass('active-next-btn');
-        };
+        } else {
+            $('#next-2').removeClass('active-next-btn');
+        }
+    };
+
+    let checkFilledInputs = () => {
+        if (!$('#customer-name').val()) {
+            $('#customer-name').siblings('.name-input-error').css('opacity', '1');
+        }
+        ;
+        if (!$('#customer-email').val()) {
+            $('#customer-email').siblings('.mail-input-error').css('opacity', '1');
+        }
+        ;
+        hideErrorMessage();
     };
 
 
-    $('.next-btn').click((e)=>{
+    ///////////CURRENT DATE CALENDAR SETTINGS/////////////////
+
+    $('#arrive-date').attr('min', `${today}`);
+
+
+    ///////////CLICK EVENTS/////////////////
+
+    $('.next-btn').click((e) => {
         checkFilledForm();
         hideErrorMessage();
         let numBtn = e.target.id.split('-')[1];
-            if(e.target.classList.contains('active-next-btn')){
-                document.getElementsByClassName('form-slide')[numBtn-1].classList.remove('active-form-slide');
-                document.getElementsByClassName('form-slide')[numBtn-1].classList.add('left-form-slide');
-                document.getElementsByClassName('form-slide')[numBtn].classList.add('active-form-slide');
-            }else{
-                if(numBtn==1){
-                    showErrorMessageFirst();
-                }
-                if(numBtn==2){
-                    showErrorMessageSecond();
-                }
+        if ($(e.target).hasClass('active-next-btn')) {
+            $(`.form-slide:eq(${numBtn - 1})`).removeClass('active-form-slide').addClass('left-form-slide');
+            $(`.form-slide:eq(${numBtn})`).addClass('active-form-slide');
+        } else {
+            if (numBtn == 1) {
+                showErrorMessageFirst();
             }
-        });
+            if (numBtn == 2) {
+                showErrorMessageSecond();
+            }
+        }
+    });
 
-    $('.prev-btn').click((e)=>{
-            let numBtn = e.target.id.split('-')[1];
-            document.getElementsByClassName('form-slide')[numBtn].classList.remove('active-form-slide');
-            document.getElementsByClassName('form-slide')[numBtn-1].classList.remove('left-form-slide');
-            document.getElementsByClassName('form-slide')[numBtn-1].classList.add('active-form-slide');
-        });
+    $('.prev-btn').click((e) => {
+        let numBtn = e.target.id.split('-')[1];
+        $(`.form-slide:eq(${numBtn})`).removeClass('active-form-slide');
+        $(`.form-slide:eq(${numBtn - 1})`).removeClass('left-form-slide').addClass('active-form-slide');
+    });
 
-    $('.form-tab').click((e)=>{
+    $('#finish-btn').click((e) => {
+        hideErrorMessage();
+        checkFilledInputs();
+    });
+
+    $('.form-tab').click((e) => {
         $(e.target).siblings().removeClass('active-form-tab');
         $(e.target).addClass('active-form-tab');
         $('.active-form-tab').parent().addClass('filled-quest');
+        $(e.target).siblings('.no-left-border-tab').removeClass('no-left-border-tab');
+        $(e.target).prev().addClass('no-left-border-tab');
         hideErrorMessage();
     });
 
-    $('.form-multi-tab').click((e)=>{
-        $(e.target).toggleClass('active-form-tab');
-        $(e.target).find('.fa-plus').toggleClass('fa-plus-turned');
-        $('.active-form-tab').parent().addClass('filled-quest');
-        hideErrorMessage();
+    $('.form-multi-tab').click((e) => {
+        if (($(e.target).hasClass('active-form-tab')) && (!$(e.target).siblings().hasClass('active-form-tab')) && ($(e.target).parent().hasClass('filled-quest'))) {
+            $(e.target).parent().removeClass('filled-quest');
+            $(e.target).removeClass('active-form-tab');
+            $(e.target).find('.fa-plus').removeClass('fa-plus-turned');
+            $(e.target).siblings('.form-error').css({'opacity': '1'});
+        } else {
+            $(e.target).parent().addClass('filled-quest');
+            $(e.target).toggleClass('active-form-tab');
+            $(e.target).find('.fa-plus').toggleClass('fa-plus-turned');
+            hideErrorMessage();
+        }
     });
 
 });
-
