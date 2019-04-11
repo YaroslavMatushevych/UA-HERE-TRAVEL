22deb307c5fabccbc9753ad9a9fed6e5676dc2c2
yaroslav <optima19977@gmail.com>
2019-04-11 11:58:33 +0300

    media 768px and new slider

--- src/sections/3_about_us/js/about-us.js
+++ src/sections/3_about_us/js/about-us.js
@@ -1,10 +1,10 @@
 let activeColor = document.getElementsByClassName("sidebar__nav-item_active");
-let christ = document.getElementsByClassName("christ");
+let navItemArrow = document.getElementsByClassName("nav-item__arrow");
 let sideBarNav = document.getElementById("sidebar__nav");
 let sideBarNavItem = document.getElementsByClassName("sidebar__nav-item");
 let personContainer = document.getElementsByClassName("person__container");
 
-christ[1].style.display = "inline-block";
+navItemArrow[1].style.display = "inline-block";
 personContainer[1].style.display = "flex";
 
 sideBarNav.addEventListener("click", function (e) {
@@ -17,8 +17,8 @@ sideBarNav.addEventListener("click", function (e) {
             activeColor[i].classList.remove("sidebar__nav-item_active");
         }
 
-        for (let l = 0; l < christ.length; l++) {
-            christ[l].style.display = "none";
+        for (let l = 0; l < navItemArrow.length; l++) {
+            navItemArrow[l].style.display = "none";
         }
 
         for (let k = 0; k < personContainer.length; k++) {
@@ -35,4 +35,47 @@ sideBarNav.addEventListener("click", function (e) {
             }
         }
     }
+});
+
+$('.arrow-next').click(function() {
+    //creating the variables so that we can target them in jQuery
+    let currentSlide = $('.person__active-slide');
+    let nextSlide = currentSlide.next();
+    let currentDot = $('.active-dot');
+    let nextDot= currentDot.next();
+    //if the current slide is the last, make the next slide the first one
+    if(nextSlide.length == 0 ) {
+        nextSlide = $('.person__container').first() ;
+    }
+    //if the next dot is the last, make the next dot the first one
+    if(nextDot.length == 0){
+        nextDot = $('.dot').first()
+    }
+    //when the arrow is clicked, fade out the current img and fade in the next one
+    currentSlide.fadeOut(600).removeClass('person__active-slide');
+    nextSlide.fadeIn(600).addClass('person__active-slide');
+    //when the arrow is clicked, remove the active class from the current dot and add it to the next one
+    currentDot.removeClass('active-dot');
+    nextDot.addClass('active-dot')
+});
+$('.arrow-prev').click(function(){
+    //creating the variables so that we can target them in jQuery
+    let currentSlide = $('.person__active-slide');
+    let prevSlide = currentSlide.prev();
+    let currentDot= $('.active-dot');
+    let prevDot= currentDot.prev();
+    //if the current slide is the last, make the prev slide the last one
+    if(prevSlide.length == 0){
+        prevSlide = $('.person__container').last();
+    }
+    //if the current dot is the last, make the prev dot the last one
+    if(prevDot.length==0){
+        prevDot = $('.dot').last();
+    }
+    //fade out the current img and fade in the prev one when the arrow is clicked
+    currentSlide.fadeOut(600).removeClass('person__active-slide');
+    prevSlide.fadeIn(600).addClass('person__active-slide');
+    //when the arrow is clicked, remove the active class from the current dot and add it to the next one
+    currentDot.removeClass('active-dot');
+    prevDot.addClass('active-dot')
 });
