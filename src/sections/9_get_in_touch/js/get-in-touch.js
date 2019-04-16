2dbeb9f91adfd7e5a71d2b0fd19bd59068931ee6
max <maxhil89@gmail.com>
2019-04-16 18:20:14 +0300

    modal added. 993 media added

--- src/sections/9_get_in_touch/js/get-in-touch.js
+++ src/sections/9_get_in_touch/js/get-in-touch.js
@@ -1,1 +1,9 @@
 console.log('get-in-touch');
+$(document).ready(function(){
+    $('.get-in-touch__btn').click(function (){
+       $('.get-in-touch__modal-overlay').addClass('get-in-touch__modal_active');}
+    );
+    $('.get-in-touch__modal-button-cross, .get-in-touch__modal-button-close').click(function () {
+        $('.get-in-touch__modal-overlay').removeClass('get-in-touch__modal_active');
+    })
+});
