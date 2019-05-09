5b738683c4d470f72a23bfe917623003c90ce192
IrynaViatkina <vis720.mail.ru>
2019-05-09 21:33:19 +0300

    rename classes over text added new images

--- src/sections/7_what_others_say/js/what-others-say.js
+++ src/sections/7_what_others_say/js/what-others-say.js
@@ -13,45 +13,51 @@ $(function(){
 });
 
 const peoplePhoto = document.getElementsByClassName("photo-item");
-const peopleSpeach = document.getElementsByClassName("people-speach");
-const peopleSpeachItem = document.getElementsByClassName("people-speach-item");
-const screens = document.getElementsByClassName("whatsapp-block-item");
-let whatsappBlock = document.getElementById("whatsapp-block");
-let overlayScreen = document.getElementById("overlay");
+const peopleSpeach = document.getElementsByClassName("speach-item");
+const peopleSpeachItem = document.getElementsByClassName("speach-item-description");
+const screens = document.getElementsByClassName("whatsapp-item");
+let whatsappBlock = document.getElementById("whatsapp-container");
+let overlayScreen = document.getElementById("wos-overlay");
 let speachHeight = 195;
 //let speachHeight = parseInt(peopleSpeachItem[0].style.lineHeight) * 100;
 
+//console.log(document.querySelector(".speach-item-description").style);
 let activeBlock = 0;
 
 (function createDots() {
     for (let i = 0; i < peoplePhoto.length; i++) {
         let wosDot = document.createElement('span');
         wosDot.classList.add('wos-dot-item');
-        document.querySelector('.wos-dots').appendChild(wosDot);
+        document.querySelector('.wos-dot').appendChild(wosDot);
     }
     document.querySelector('.wos-dot-item').classList.add('wos-dot-item-active');
+})();
+
+(function addOver(){
+    if(peopleSpeachItem[activeBlock].scrollHeight > speachHeight){
+        peopleSpeachItem[activeBlock].classList.add("speach-item-description-over");
+    }
 })();
 
 const wosDots = document.getElementsByClassName("wos-dot-item");
 
 document.addEventListener('click', function(e){
-    if(e.target.id === 'right-arrow' || e.target.id === 'right-arrow-screen'){
+    if(e.target.id === 'right-arrow' || e.target.id === 'right-arrow-whatsapp'){
         moveRight();
     }
-    if(e.target.id === 'left-arrow' || e.target.id === 'left-arrow-screen'){
+    if(e.target.id === 'left-arrow' || e.target.id === 'left-arrow-whatsapp'){
         moveLeft();
     }
     if(e.target.className === "whatsapp-btn"){
         whatsappBlock.hidden = false;
         overlayScreen.hidden = false;
     }
-    if(e.target.id === "close-icon" || e.target.id === "overlay"){
+    if(e.target.id === "close-icon" || e.target.id === "wos-overlay"){
         whatsappBlock.hidden = true;
         overlayScreen.hidden = true;
     }
-    if(e.target.classList.contains("people-speach-item-over")){
-        console.log('ok');
-        e.target.classList.remove("people-speach-item-over");
+    if(e.target.classList.contains("speach-item-description-over")){
+        e.target.classList.remove("speach-item-description-over");
     }
 });
 
@@ -78,8 +84,8 @@ function moveRight(){
 
 function movePhotoBlock(activeItem, hiddenItem){
     peoplePhoto[hiddenItem].classList.toggle('photo-item-fade');
-    peopleSpeach[hiddenItem].classList.toggle('people-speach-fade');
-    screens[hiddenItem].classList.toggle('whatsapp-block-item-fade');
+    peopleSpeach[hiddenItem].classList.toggle('speach-item-fade');
+    screens[hiddenItem].classList.toggle('whatsapp-item-fade');
     wosDots[hiddenItem].classList.toggle('wos-dot-item-active');
     wosDots[activeItem].classList.toggle('wos-dot-item-active');
 
@@ -91,15 +97,15 @@ function movePhotoBlock(activeItem, hiddenItem){
         peoplePhoto[activeItem].hidden = false;
         peopleSpeach[activeItem].hidden = false;
         if(peopleSpeachItem[activeItem].scrollHeight > speachHeight){
-            peopleSpeachItem[activeItem].classList.add("people-speach-item-over");
+            peopleSpeachItem[activeItem].classList.add("speach-item-description-over");
         }
 
         screens[activeItem].hidden = false;
     }, 200);
     setTimeout(function(){
         peoplePhoto[activeItem].classList.toggle('photo-item-fade');
-        peopleSpeach[activeItem].classList.toggle('people-speach-fade');
-        screens[activeItem].classList.toggle('whatsapp-block-item-fade');
+        peopleSpeach[activeItem].classList.toggle('speach-item-fade');
+        screens[activeItem].classList.toggle('whatsapp-item-fade');
     }, 300);
 }
 
