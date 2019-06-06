c7dfd5d2c95918e61c135710d32a896cace50dc6
max <maxhil89@gmail.com>
2019-06-06 21:29:37 +0300

    burger menu fixed

--- src/sections/1_header/js/header.js
+++ src/sections/1_header/js/header.js
@@ -64,13 +64,13 @@ burger.addEventListener('click',function () {
     let bItems = document.getElementById('burgerItems');
     let children = document.querySelectorAll(" div.navbar, a.anchors-a, a.anchors-a-active, img.logo, div.nav-wrapper, div.block a.s-anchors, div.social-media");
 
-    if (!toursExist) {
-        newNavElement.innerHTML = 'Tours';
-        newNavElement.className += ' anchors-a ';
-        bItems.appendChild(newNavElement);
-
-        toursExist = true;
-    }
+    // if (!toursExist) {
+    //     newNavElement.innerHTML = 'Tours';
+    //     newNavElement.className += ' anchors-a ';
+    //     bItems.appendChild(newNavElement);
+    //
+    //     toursExist = true;
+    // }
     for ( let i = 0; i<children.length; i++){
         let child = children[i];
         // let freeGuide = children[i=2];
@@ -115,3 +115,75 @@ window.addEventListener("resize", function () {
     window.addEventListener('scroll', trackScroll);
     goTopBtn.addEventListener('click', backToTop);
 })();
+
+// ***smooth scroll test
+// let mainNavLinks = document.querySelectorAll(".anchors-a,  .anchors-a-active");
+// let mainSections = document.querySelector("section");
+//
+// let lastId;
+// let cur = [];
+//
+// window.addEventListener("scroll", event => {
+//   let fromTop = window.scrollY;
+//
+//   mainNavLinks.forEach(link => {
+//     let section = document.querySelector(link.hash);
+//
+//     if (
+//       section.offsetTop <= fromTop &&
+//       section.offsetTop + section.offsetHeight > fromTop
+//     ) {
+//       link.classList.add("current");
+//     } else {
+//       link.classList.remove("current");
+//     }
+//   });
+// });
+//****
+
+$('.burger-items>a').click((e) => {
+    let clicked = $(e.target).index();
+    switch (clicked) {
+        case 0:
+            $(document.documentElement).animate({
+                scrollTop: $('#about-us').offset().top - 60
+            }, 1000);
+            break;
+        case 1:
+            $(document.documentElement).animate({
+                scrollTop: $('#free-guide').offset().top - 60
+            }, 1000);
+            break;
+        case 2:
+            $(document.documentElement).animate({
+                scrollTop: $('#routes').offset().top - 60
+            }, 1000);
+            break;
+        case 3:
+            $(document.documentElement).animate({
+                scrollTop: $('#others-say').offset().top - 60
+            }, 1000);
+            break;
+        case 4:
+            $(document.documentElement).animate({
+                scrollTop: $('#slider').offset().top - 60
+            }, 1000);
+            break;
+        case 5:
+            $(document.documentElement).animate({
+                scrollTop: $('#get-in-touch').offset().top - 60
+            }, 1000);
+            break;
+        case 6:
+            $(document.documentElement).animate({
+                scrollTop: $('#individual-tour').offset().top - 60
+            }, 1000);
+            break;
+        default:
+            $(document.documentElement).animate({
+                scrollTop: $(document.body).offset().top
+            }, 1000);
+    }
+});
+
+});
